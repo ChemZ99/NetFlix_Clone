2b83cbc34cb649974bf3510c67e3a2cc5e8f8bcb
Andrea Cavallo <astarore@gmail.com>
2024-05-03 16:41:00 +0200

    working on details modal

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,4 +1,5 @@
-import { Container } from "react-bootstrap"
+import { Button, Container } from "react-bootstrap"
+import { InfoCircle, PlayFill } from "react-bootstrap-icons"
 
 const HeroSection = () => {
         return (
@@ -8,6 +9,9 @@ const HeroSection = () => {
                  <p className="text-white mt-3 lh-1">Better Call Saul è una serie televisiva statunitense ideata da</p>
                  <p className="text-white lh-1"> Vince Gilligan e Peter Gould e trasmessa sul canale  </p>
                  <p className="text-white lh-1">via cavo AMC dall'8 febbraio 2015 al 15 agosto 2022</p>
+                 <div>
+                    <Button variant="light" size="lg" className="me-2 ps-4 pe-4"><PlayFill size={30}/> Play</Button> <Button variant="secondary" size="lg" className="ps-4 pe-4"><InfoCircle size={30} className="me-2"/>    More Info</Button>
+                 </div>
             </div>
         )
 }
--- src/components/NetflixSlider/ShowDetailsButton.js
+++ src/components/NetflixSlider/ShowDetailsButton.js
@@ -1,12 +1,25 @@
 import React from 'react';
 import IconArrowDown from './../Icons/IconArrowDown'
 import './ShowDetailsButton.scss'
+import { Button } from 'react-bootstrap';
+import { ChevronDown, Dot, HandThumbsUp, PlayFill, Plus } from 'react-bootstrap-icons';
 
 const ShowDetailsButton = ({ onClick }) => (
   <button onClick={onClick} className="show-details-button">
-    <span>
-      <IconArrowDown />
-    </span>
+    <div>
+      <div className='d-flex justify-content-between'>
+        <div>
+          <Button variant="light" size="sm" className='rounded-circle'><PlayFill/></Button>
+          <Button variant="secondary" size="sm" className='rounded-circle'><Plus/></Button>
+          <Button variant="secondary" size="sm" className='rounded-circle'><HandThumbsUp/></Button>
+          </div>
+          <div>
+            <Button variant="secondary" size="sm" className='rounded-circle'><ChevronDown/></Button>
+            </div>
+            </div>
+            <p><span className='text-success fw-bold d-inline'>92% Match</span> <span className='border border-4 border-secondary d-inline'>18+</span> <span className='d-inline'>Limited Series</span> <span className='border border-4 border-secondary d-inline'>HD</span></p> 
+            <p><span className='d-inline'>Offbeat</span><Dot/><span className='d-inline'>Psychological</span><Dot/><span className='d-inline'>Dark Comedy</span></p>
+            </div>
   </button>
 );
 
